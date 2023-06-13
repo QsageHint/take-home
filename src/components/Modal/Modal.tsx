78338d6fed5c983b1a8ad1c87d8a8a5d1e59a995
enthusiastica <williamcambell682@gmail.com>
2023-06-12 22:51:15 -0600

    feat: :sparkles: add Form in modal

--- src/components/Modal/Modal.tsx
+++ src/components/Modal/Modal.tsx
@@ -1,9 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Modal.css';
 import { useModalContext } from '../Hook/useModalContext';
 
+
 const Modal: React.FC = () => {
   const { isModalOpen, closeModal } = useModalContext();
+
+  const [first_name, setfirstName] = useState("");
+  const [last_name, setlastName] = useState("");
+  const [photourl, setPhotoUrl] = useState("");
+  const [agentLicence, setAgentLicence] = useState("");
+  const [address, setAddress] = useState("");
+  const [practiceAreas, setPracticeAreas] = useState<string[]>([]);
+  const [aboutMe, setAboutMe] = useState("");
+
+  const handleAdd = (event: React.KeyboardEvent<HTMLInputElement>) => {
+    const newPracticeArea: string = event.currentTarget.value;
+    if (event.key === "Enter") {
+      setPracticeAreas([...practiceAreas, newPracticeArea]);
+      event.currentTarget.value = "";
+    }
+  };
+
+  const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+  };
 
   return (
     <>
@@ -29,7 +50,84 @@ const Modal: React.FC = () => {
               >&times;</button>
             </div>
             <div className="modal-body">
-              
+              <form onSubmit={handleSubmit} >
+                <div className="form-group">
+                  <label htmlFor="first_name">First Name</label>
+                  <input
+                    type="text"
+                    id="first_name"
+                    className="form-control"
+                    value={first_name}
+                    onChange={(event) => setfirstName(event.target.value)}
+                  />
+                </div>
+                <div className="form-group">
+                  <label htmlFor="last_name">Last Name</label>
+                  <input
+                    type="text"
+                    id="last_name"
+                    className="form-control"
+                    value={last_name}
+                    onChange={(event) => setlastName(event.target.value)}
+                  />
+                </div>
+                <div className="form-group">
+                  <label htmlFor="photourl">Photo URL</label>
+                  <input
+                    type="text"
+                    id="photourl"
+                    className="form-control"
+                    value={photourl}
+                    onChange={(event) => setPhotoUrl(event.target.value)}
+                  />
+                </div>
+                <div className="form-group">
+                  <label htmlFor="agentLicence">Agent Licence</label>
+                  <input
+                    type="text"
+                    id="agentLicence"
+                    className="form-control"
+                    value={agentLicence}
+                    onChange={(event) => setAgentLicence(event.target.value)}
+                  />
+                </div>
+                <div className="form-group">
+                  <label htmlFor="address">Address</label>
+                  <input
+                    type="text"
+                    id="address"
+                    className="form-control"
+                    value={address}
+                    onChange={(event) => setAddress(event.target.value)}
+                  />
+                </div>
+                <div className="form-group">
+                  <label htmlFor="practiceAreas">Practice Areas (use Enter to seperate areas)</label>
+                  <input
+                    type="text"
+                    id="practiceAreas"
+                    className="form-control"
+                    onKeyDown={handleAdd}
+                  />
+                  <ul className="list-inline">
+                    {practiceAreas.map((practiceArea) => (
+                      <li className="list-inline-item badge badge-primary">{practiceArea}</li>
+                    ))}
+                  </ul>
+                </div>
+                <div className="form-group">
+                  <label htmlFor="aboutMe">About Me</label>
+                  <textarea
+                    id="aboutMe"
+                    className="form-control"
+                    value={aboutMe}
+                    onChange={(event) => setAboutMe(event.target.value)}
+                  />
+                </div>
+                <button type="submit" className="btn btn-primary" >
+                  Save Change
+                </button>
+              </form>
             </div>
           </div>
         </div>
